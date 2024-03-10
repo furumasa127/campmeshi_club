5bbb83c3d810abd07a137816b07129e56645f907
EC2 Default User <149689018+furumasa127@users.noreply.github.com>
2024-03-10 03:07:15 +0000

    [Finish]会員詳細のレスポンシブ

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,14 +1,14 @@
 class Public::SearchesController < ApplicationController
   def genre_search
     @genre_id = params[:genre_id]
-    @recipes = Recipe.published.where(genre_id: @genre_id)
+       @recipes = Recipe.published.where(genre_id: @genre_id).page(params[:page]).per(10)
     @genres = Genre.all
   end
   
   def search
     @content = params[:content]
     @method = params[:method]
-    @records = Recipe.published.search_for(@content, @method)
+    @records = Recipe.published.search_for(@content, @method).page(params[:page]).per(10)
     @genres = Genre.all
   end
       
